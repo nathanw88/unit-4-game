47c4974dede82a5d5ce0bffb888a4da18c5cdb3c
Nate <33041144+nathanw88@users.noreply.github.com>
2018-11-12 18:16:13 -0600

    added psuedo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,69 @@
+// step1
+
+
+// declare global variables
+// player health
+// player starting attack power
+//player attack power
+// choosen enemy hp
+// choosen enemy counter attack power
+// enemy choose start as false
+
+
+// step 2
+
+
+//create an object 
+
+//make objects filled with info for each character
+
+//create function for attacking
+
+// deal damage to selected enemy
+
+// deal damage to players health
+
+// check player health above 0 
+
+// if not alert loss 
+//reveal restart button
+
+//check if choosen enemies health is equal to or below zero 
+
+// if so hide enemy, change enemy choosen as true
+
+// add starting attack power to current attack power
+
+// print to screen updated health
+
+
+// step 3
+
+
+//make on click for players choice
+
+//hide characters not choosen
+
+// reveal characters not choosen as enemies 
+
+// step 4
+
+// make on click for chosen enemy to fight
+
+//checks if isEnemychoosen is false if so otherwhise ignore click
+
+
+//hide choosen enemy 
+
+// switch isEnemychoosen to true
+
+// reveal choosen enemy as a defender
+
+
+//step 5
+
+
+//make on click for attack button
+
+// call attack function
+
